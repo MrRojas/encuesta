833dc26a8a6b4f49608d61240817d735a3af78de
Armando Rojas <armando.rojas@coposa.com>
2020-02-04 16:57:56 -0400

    descartar livereload

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,4 +1,3 @@
-<script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
